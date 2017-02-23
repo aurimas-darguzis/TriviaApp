3c5ebbb96c1f6e10abecd7a806f4bfe5cf015835
aurimas-darguzis <aurimas@darguzis.com>
2017-02-23 20:03:45 +0000

    added question-add-update c + forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule  } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { routes } from './app.route';
 import { AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent } from './components';
 import { CategoryService, TagService, QuestionService } from './services';
+import { QuestionAddUpdateComponent } from './components/question/question-add-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent
+    AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent, QuestionAddUpdateComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +24,7 @@ import { CategoryService, TagService, QuestionService } from './services';
     // Flex
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [
