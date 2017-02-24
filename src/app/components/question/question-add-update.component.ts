e2e03db1170bcdeef56280608a39441821795c01
aurimas-darguzis <aurimas@darguzis.com>
2017-02-24 10:02:21 +0000

    started on addUpdate component. added properties. working on methods now

--- src/app/components/question/question-add-update.component.ts
+++ src/app/components/question/question-add-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormArray, FormControl, ValidatorFn, Validators  } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Category, Question, Answer } from '../../model';
+import { CategoryService, TagService, QuestionService } from '../../services';
 
 @Component({
   selector: 'app-question-add-update',
@@ -6,6 +11,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question-add-update.component.scss']
 })
 export class QuestionAddUpdateComponent implements OnInit {
+
+  // Properties
+  categories: Category[];
+  sub: any;
+
+  tags: string[];
+  sub2: any;
+
+  questionForm: FormGroup;
+  question: Question;
+
+  autoTags: string[] = []; // auto computed based on match within Q/A
+  enteredTags: string[] = [];
+
+
 
   constructor() { }
 
