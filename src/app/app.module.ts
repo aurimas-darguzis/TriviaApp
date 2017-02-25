b1eefc8c9d8f28f912131e7905c930595de7a9ac
aurimas-darguzis <aurimas@darguzis.com>
2017-02-25 14:27:06 +0000

    updated few packages versions. I mean - why not?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule  } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }     from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import 'hammerjs';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { routes } from './app.route';
-import {  AppComponent, CategoriesComponent, TagsComponent,
-          QuestionsComponent, QuestionAddUpdateComponent } from './components';
-
-import { CategoryService, TagService, QuestionService } from './services';
-
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
-import { CategoryActions, TagActions, QuestionActions } from './store/actions';
-import { CategoryEffects, TagEffects, QuestionEffects } from './store/effects';
+import { routes } from './app.route';
+import { AppComponent, CategoriesComponent, TagsComponent, 
+         QuestionsComponent, QuestionAddUpdateComponent } from './components';
+import { CategoryService, TagService, QuestionService } from './services';
+
+import {CategoryActions, TagActions, QuestionActions} from './store/actions';
+import {CategoryEffects, TagEffects, QuestionEffects} from './store/effects';
 import { default as reducer } from './store/app-store';
-
 
 @NgModule({
   declarations: [
-    AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent, QuestionAddUpdateComponent
+    AppComponent, CategoriesComponent, TagsComponent, 
+    QuestionsComponent, QuestionAddUpdateComponent
   ],
   imports: [
     BrowserModule,
     // Router
     RouterModule.forRoot(routes),
-    // Material
-    MaterialModule,
-    // Flex
-    FlexLayoutModule,
     // Forms
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
+
+    // Material
+    MaterialModule,
+    // Flex
+    FlexLayoutModule,
+
     // store
     StoreModule.provideStore(reducer),
+
+    StoreDevtoolsModule.instrumentOnlyWithExtension({
+      maxAge: 20
+    }),
+
     // ngrx effects
     EffectsModule.run(CategoryEffects),
     EffectsModule.run(TagEffects),
     EffectsModule.run(QuestionEffects)
+
   ],
   providers: [
     CategoryService, TagService, QuestionService,
     CategoryActions, TagActions, QuestionActions
+
   ],
   bootstrap: [AppComponent]
 })
