ef98e0ff29d02d7f29079771b7d8709789bf3de1
aurimas-darguzis <aurimas@darguzis.com>
2017-02-25 13:33:18 +0000

    redirect on save on app component

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,25 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { AppStore } from '../../store/app-store';
 import { CategoryActions, TagActions, QuestionActions } from '../../store/actions';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'trivia app!';
+  sub: any;
 
   constructor(private categoryActions: CategoryActions,
               private tagActions: TagActions,
               private questionActions: QuestionActions,
-              private store: Store<AppStore>) {}
+              private store: Store<AppStore>,
+              private router: Router,
+              private snackBar: MdSnackBar) {
+
+    this.sub = store.select(s => s.questionSaveStatus)
+                    .filter(status => status === 'SUCCESS')
+                    .subscribe(() => {
+                                      this.snackBar.open('Quesiont saved!', '', {duration: 2000});
+                                      this.router.navigate(['/questions']);
+                                    }
+                              )
+              }
 
   ngOnInit() {
     this.store.dispatch(this.categoryActions.loadCategories());
     this.store.dispatch(this.tagActions.loadTags());
     this.store.dispatch(this.questionActions.loadQuestions());
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 }
