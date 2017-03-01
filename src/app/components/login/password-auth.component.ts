1f31fe5f377833fbb024128f5bb42ae1cb8d9b4d
aurimas-darguzis <aurimas@darguzis.com>
2017-03-01 21:07:00 +0000

    started to build password-auth component class

---
+++ src/app/components/login/password-auth.component.ts
@@ -0,0 +1,26 @@
+
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators, FormArray, FormControl, ValidatorFn } from '@angular/forms';
+import { MdDialogRef } from '@angular/material';
+import * as firebase from 'firebase';
+import { AngularFire, AuthMethods, FirebaseAuthState } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+
+import { AppStore } from '../../store/app-store';
+
+const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+@Component({
+    selector: 'login',
+    templateUrl: './password-auth.component.html',
+    styleUrls: ['./password-auth.component.scss']
+})
+export class PasswordAuthComponent implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {
+        
+    }
+}
